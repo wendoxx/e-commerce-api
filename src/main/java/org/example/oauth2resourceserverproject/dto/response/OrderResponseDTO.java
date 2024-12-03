e044c0be0f499d841f79c47aa959d1bd2b008273
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-02 23:17:42 -0300

    feat: add constructor with parameter Order

--- src/main/java/org/example/oauth2resourceserverproject/dto/response/OrderResponseDTO.java
+++ src/main/java/org/example/oauth2resourceserverproject/dto/response/OrderResponseDTO.java
@@ -1,6 +1,7 @@
 package org.example.oauth2resourceserverproject.dto.response;
 
 import lombok.Data;
+import org.example.oauth2resourceserverproject.model.Order;
 
 import java.time.LocalDate;
 
@@ -11,4 +12,12 @@ public class OrderResponseDTO {
     private LocalDate expectedDate;
     private String buyer;
     private String seller;
+
+    public OrderResponseDTO(Order order) {
+        this.id = order.getId();
+        this.products = getProducts();
+        this.expectedDate = getExpectedDate();
+        this.buyer = getBuyer();
+        this.seller = getSeller();
+    }
 }
