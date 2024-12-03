a490ff66a789895f77e441743fbec8417f0b7ed0
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-02 22:56:17 -0300

    refactor: turn a method find by id from Product to Optional

--- src/main/java/org/example/oauth2resourceserverproject/reporitory/ProductRepository.java
+++ src/main/java/org/example/oauth2resourceserverproject/reporitory/ProductRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByName(String name);
+    Optional<Product> findByName(String name);
     List<Product> findBySoldBy(String soldBy);
+
 }
