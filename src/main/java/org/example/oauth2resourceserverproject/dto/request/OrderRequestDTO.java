c4403107b9bcb8272ba959ed9caf06ceb5e0ff07
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-06 17:05:26 -0300

    fix: correct product attribute type in OrderRequestDTO

--- src/main/java/org/example/oauth2resourceserverproject/dto/request/OrderRequestDTO.java
+++ src/main/java/org/example/oauth2resourceserverproject/dto/request/OrderRequestDTO.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 public class OrderRequestDTO {
     private Long id;
-    private List<String> products;
+    private Long product;
     @JsonFormat(pattern = "dd-mm-yyyy")
     private LocalDate expectedDate;
     private String buyer;
