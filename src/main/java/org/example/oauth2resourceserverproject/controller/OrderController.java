4abdff28af4fa7cc7ea779d87355c5fae1df2283
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-06 18:56:41 -0300

    feat: add order controller with all CRUD methods

--- src/main/java/org/example/oauth2resourceserverproject/controller/OrderController.java
+++ src/main/java/org/example/oauth2resourceserverproject/controller/OrderController.java
@@ -1,17 +1,45 @@
 package org.example.oauth2resourceserverproject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.oauth2resourceserverproject.dto.request.OrderRequestDTO;
+import org.example.oauth2resourceserverproject.dto.response.OrderResponseDTO;
+import org.example.oauth2resourceserverproject.model.Order;
+import org.example.oauth2resourceserverproject.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
 
-    @GetMapping("/public")
-    public String getById(){
-        return "it get any order by id.";
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/public/get-order/{id}")
+    public ResponseEntity<OrderResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.findById(id));
     }
 
+    @GetMapping("/private/all-orders")
+    public ResponseEntity<List<OrderResponseDTO>> getAllOrders() {
+        return ResponseEntity.ok(orderService.findAllOrders());
+    }
 
+    @PostMapping("/public/create-order")
+    public ResponseEntity<Order> saveOrder(OrderRequestDTO orderRequestDTO) {
+        return ResponseEntity.status(201).body(orderService.saveAndUpdateOrder(orderRequestDTO));
+    }
+
+    @PutMapping("/public/update-order")
+    public ResponseEntity<Order> updateOrder(OrderRequestDTO orderRequestDTO) {
+        return ResponseEntity.status(201).body(orderService.saveAndUpdateOrder(orderRequestDTO));
+    }
+
+    @DeleteMapping("/public/delete-order/{id}")
+    public ResponseEntity<OrderResponseDTO> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
+    }
 }
