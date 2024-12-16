4ae1da304c0d11ddc86475f91921658027315981
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-16 17:02:30 -0300

    fix: currect attributes product, expectedDate and buyer in OrderResponseDTO constructor

--- src/main/java/org/example/oauth2resourceserverproject/dto/response/OrderResponseDTO.java
+++ src/main/java/org/example/oauth2resourceserverproject/dto/response/OrderResponseDTO.java
@@ -1,22 +1,29 @@
 package org.example.oauth2resourceserverproject.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.example.oauth2resourceserverproject.model.Order;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 public class OrderResponseDTO {
     private Long id;
-    private List<ProductResponseDTO> product;
+    private Set<ProductResponseDTO> product;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate expectedDate;
     private String buyer;
 
     public OrderResponseDTO(Order order) {
         this.id = order.getId();
-        this.product = getProduct();
-        this.expectedDate = getExpectedDate();
-        this.buyer = getBuyer();
+        this.product = order.getProduct() != null
+                ? order.getProduct().stream()
+                .map(ProductResponseDTO::new)
+                .collect(Collectors.toSet())
+                : null;
+        this.expectedDate = order.getExpectedDate();
+        this.buyer = order.getBuyer();
     }
 }
