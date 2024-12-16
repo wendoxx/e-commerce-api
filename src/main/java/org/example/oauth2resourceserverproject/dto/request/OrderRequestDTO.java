412e3a994efdec534d27d10fdfc98c06a41ba618
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-16 17:00:36 -0300

    fix: correct attribute product and JsonFormat pattern in OrderRequestDTO

--- src/main/java/org/example/oauth2resourceserverproject/dto/request/OrderRequestDTO.java
+++ src/main/java/org/example/oauth2resourceserverproject/dto/request/OrderRequestDTO.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class OrderRequestDTO {
     private Long id;
-    private Long product;
-    @JsonFormat(pattern = "dd-mm-yyyy")
+    private Set<Long> product;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate expectedDate;
     private String buyer;
-    private String seller;
 }
