363a634a71d6b9bf8f873c7a8ec9a225e56f0cb8
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-12-21 19:48:37 -0300

    feat: correct method to save and update orders and add method to calculate total

--- src/main/java/org/example/oauth2resourceserverproject/service/OrderService.java
+++ src/main/java/org/example/oauth2resourceserverproject/service/OrderService.java
@@ -4,12 +4,15 @@ import jakarta.transaction.Transactional;
 import org.example.oauth2resourceserverproject.dto.request.OrderRequestDTO;
 import org.example.oauth2resourceserverproject.dto.response.OrderResponseDTO;
 import org.example.oauth2resourceserverproject.model.Order;
+import org.example.oauth2resourceserverproject.model.Product;
 import org.example.oauth2resourceserverproject.reporitory.OrderRepository;
 import org.example.oauth2resourceserverproject.reporitory.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,12 +24,20 @@ public class OrderService {
     @Autowired
     ProductRepository productRepository;
 
-    public OrderResponseDTO findById(Long id) {
+    public OrderResponseDTO findById(UUID id) {
         return orderRepository.findById(id).map(OrderResponseDTO::new).orElseThrow(() -> new RuntimeException("Order not found."));
     }
 
     public List<OrderResponseDTO> findAllOrders() {
         return orderRepository.findAll().stream().map(OrderResponseDTO::new).toList();
+    }
+
+    private Double calculateTotal(OrderRequestDTO orderRequestDTO) {
+        double total = 0;
+        for (UUID productId : orderRequestDTO.getProducts()) {
+            total += productRepository.findById(productId).orElseThrow(() -> new RuntimeException("Product not found.")).getPrice();
+        }
+        return total;
     }
 
     @Transactional
@@ -39,13 +50,22 @@ public class OrderService {
             order = new Order();
         }
 
+        Set<Product> products = orderRequestDTO.getProducts()
+                .stream()
+                .map(productId -> productRepository
+                        .findById(productId)
+                        .orElseThrow(() -> new RuntimeException("Product not found.")))
+                .collect(Collectors.toSet());
+
+
+        order.setTotal(calculateTotal(orderRequestDTO));
         order.setBuyer(orderRequestDTO.getBuyer());
         order.setExpectedDate(orderRequestDTO.getExpectedDate());
-        order.setProduct(orderRequestDTO.getProduct().stream().map(product -> productRepository.findById(product).orElseThrow(() -> new RuntimeException("Product not found."))).collect(Collectors.toSet()));
+        order.setProducts(products);
         return orderRepository.save(order);
     }
 
-    public void deleteOrder(Long id) {
+    public void deleteOrder(UUID id) {
         orderRepository.deleteById(id);
     }
 }
