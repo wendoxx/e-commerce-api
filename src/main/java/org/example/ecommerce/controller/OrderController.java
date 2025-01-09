c50b36b774493c0df9e1bf329171b50124ed9a6e
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-09 19:45:58 -0300

    feat: implement swagger

--- src/main/java/org/example/ecommerce/controller/OrderController.java
+++ src/main/java/org/example/ecommerce/controller/OrderController.java
@@ -1,5 +1,8 @@
 package org.example.ecommerce.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.ecommerce.dto.request.OrderRequestDTO;
 import org.example.ecommerce.dto.response.OrderResponseDTO;
 import org.example.ecommerce.model.Order;
@@ -13,31 +16,73 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/order")
+@Tag(name = "Order Controller", description = "This controller is responsible for handling order related operations.")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
+    @Operation(
+            method = "GET",
+            summary = "Get an order by id",
+            description = "This endpoint retrieves an order by its id"
+    )
+    @ApiResponse(responseCode = "200", description = "Order retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Order not found")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/get-order-id/{id}")
     public ResponseEntity<OrderResponseDTO> getById(@PathVariable UUID id) {
         return ResponseEntity.ok(orderService.findById(id));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get all orders",
+            description = "This endpoint retrieves all orders"
+    )
+    @ApiResponse(responseCode = "200", description = "Orders retrieved successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/all-orders")
     public ResponseEntity<List<OrderResponseDTO>> getAllOrders() {
         return ResponseEntity.ok(orderService.findAllOrders());
     }
 
+    @Operation(
+            method = "POST",
+            summary = "Save order",
+            description = "This endpoint saves an order"
+    )
+    @ApiResponse(responseCode = "201", description = "Order saved successfully")
+    @ApiResponse(responseCode = "400", description = "Bad Request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/public/save-order")
     public ResponseEntity<Order> saveOrder(@RequestBody OrderRequestDTO orderRequestDTO) {
         return ResponseEntity.status(201).body(orderService.saveAndUpdateOrder(orderRequestDTO));
     }
 
+    @Operation(
+            method = "PUT",
+            summary = "Update order",
+            description = "This endpoint updates an order"
+    )
+    @ApiResponse(responseCode = "201", description = "Order updated successfully")
+    @ApiResponse(responseCode = "400", description = "Bad Request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PutMapping("/public/update-order")
     public ResponseEntity<Order> updateOrder(@RequestBody OrderRequestDTO orderRequestDTO) {
         return ResponseEntity.status(201).body(orderService.saveAndUpdateOrder(orderRequestDTO));
     }
 
+    @Operation(
+            method = "DELETE",
+            summary = "Delete an order by id",
+            description = "This endpoint deletes an order"
+    )
+    @ApiResponse(responseCode = "204", description = "No content")
+    @ApiResponse(responseCode = "404", description = "Order not found")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @DeleteMapping("/public/delete-order/{id}")
     public ResponseEntity<OrderResponseDTO> deleteOrder(@PathVariable UUID id) {
         orderService.deleteOrder(id);
--- src/main/java/org/example/ecommerce/controller/ProductController.java
+++ src/main/java/org/example/ecommerce/controller/ProductController.java
@@ -1,5 +1,8 @@
 package org.example.ecommerce.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.ecommerce.dto.request.ProductRequestDTO;
 import org.example.ecommerce.dto.response.ProductResponseDTO;
 import org.example.ecommerce.model.Product;
@@ -13,42 +16,103 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@Tag(name = "Product Controller", description = "This controller is responsible for handling order related operations.")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
+    @Operation(
+            method = "GET",
+            summary = "Get a product by id",
+            description = "This endpoint retrieves a product by its id"
+    )
+    @ApiResponse(responseCode = "200", description = "Product retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/get-product-id/{id}")
     public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable UUID id) {
         return ResponseEntity.ok(productService.findById(id));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get a product by name",
+            description = "This endpoint retrieves a product by its name"
+    )
+    @ApiResponse(responseCode = "200", description = "Product retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/product-name")
     public ResponseEntity<ProductResponseDTO> getProductByName(@RequestParam String name) {
         return ResponseEntity.ok(productService.findByName(name));
     }
 
+    @Operation(
+            method = "GET",
+            summary = "List products by store",
+            description = "This endpoint retrieves a list of all products by its store"
+    )
+    @ApiResponse(responseCode = "200", description = "Product retrieved successfully")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/product-by-store")
     public ResponseEntity<List<ProductResponseDTO>> getProductByStore(String soldBy) {
         return ResponseEntity.ok(productService.findByStore(soldBy));
 
     }
 
+    @Operation(
+            method = "GET",
+            summary = "Get all products",
+            description = "This endpoint retrieves a list of all products"
+    )
+    @ApiResponse(responseCode = "200", description = "Product retrieved successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @GetMapping("/public/all-products")
     public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
         return ResponseEntity.ok(productService.findAllProducts());
     }
 
+    @Operation(
+            method = "POST",
+            summary = "Save a product",
+            description = "This endpoint saves a product"
+    )
+    @ApiResponse(responseCode = "201", description = "Product saved successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PostMapping("/private/save-product")
     public ResponseEntity<Product> saveProduct(@RequestBody ProductRequestDTO productRequestDTO) {
         return ResponseEntity.status(201).body(productService.saveAndUpdateProduct(productRequestDTO));
     }
 
+    @Operation(
+            method = "PUT",
+            summary = "Update a product",
+            description = "This endpoint updates a product"
+    )
+    @ApiResponse(responseCode = "201", description = "Product updated successfully")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @PutMapping("/private/update-product")
     public ResponseEntity<Product> updateProduct(@RequestBody ProductRequestDTO productRequestDTO) {
         return ResponseEntity.status(201).body(productService.saveAndUpdateProduct(productRequestDTO));
     }
 
+    @Operation(
+            method = "DELETE",
+            summary = "Delete a product by id",
+            description = "This endpoint deletes a product by its id"
+    )
+    @ApiResponse(responseCode = "204", description = "No content")
+    @ApiResponse(responseCode = "404", description = "Product not found")
+    @ApiResponse(responseCode = "400", description = "Bad request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     @DeleteMapping("/private/delete-product{id}")
     public ResponseEntity<ProductResponseDTO> deleteProduct(@PathVariable UUID id) {
         productService.deleteProduct(id);
