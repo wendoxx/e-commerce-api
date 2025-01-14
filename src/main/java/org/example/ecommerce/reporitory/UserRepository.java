0e3b1fdc1f5396c3053744190de4b2835e87537e
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-14 18:50:19 -0300

    feat: add user repository

---
+++ src/main/java/org/example/ecommerce/reporitory/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example.ecommerce.reporitory;
+
+import org.example.ecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByUsername(String username);
+}
