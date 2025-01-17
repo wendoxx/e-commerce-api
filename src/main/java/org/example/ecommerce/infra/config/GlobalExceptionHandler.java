65762e7d7c695af06ba425cd4aabd1e9a1a67468
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-17 20:47:57 -0300

    feat: add globalExceptionHandler

---
+++ src/main/java/org/example/ecommerce/infra/config/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.example.ecommerce.infra.config;
+
+import org.example.ecommerce.infra.config.exception.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public String handleOrderNotFoundException(OrderNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public String handleProductNotFoundException(ProductNotFoundException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(OrderListIsEmptyException.class)
+    public String handleOrderListIsEmptyException(OrderListIsEmptyException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(ProductListIsEmptyException.class)
+    public String handleProductListEmptyException(ProductListIsEmptyException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public String handleBadRequestException(BadRequestException e) {
+        return e.getMessage();
+    }
+}
