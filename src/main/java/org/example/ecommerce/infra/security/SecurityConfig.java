5b296efadc4ef5538e4b5dab62de5b2723df3cb5
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-16 22:48:30 -0300

    fix: disable csrf, add sessionManagement and correct roles

--- src/main/java/org/example/ecommerce/infra/security/SecurityConfig.java
+++ src/main/java/org/example/ecommerce/infra/security/SecurityConfig.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,16 +21,21 @@ public class SecurityConfig {
     SecurityFilter securityFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher) throws Exception {
-        return http.authorizeHttpRequests((authorize) -> authorize
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf((csrf -> csrf.disable()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("api/login").permitAll()
                         .requestMatchers("api/register").permitAll()
+                        .requestMatchers("swagger-ui/index.html/**").permitAll()
                         .requestMatchers("api/v1/order/public/**").permitAll()
                         .requestMatchers("api/v1/product/private/").authenticated()
-                        .requestMatchers("api/v1/product/private/save-product").hasRole("ROLE_ADMIN")
-                        .requestMatchers("api/v1/product/private/update-product").hasRole("ROLE_ADMIN")
-                        .requestMatchers("api/v1/order/private/save-order").hasRole("ROLE_ADMIN")
-                        .requestMatchers("api/v1/order/private/update-order").hasRole("ROLE_ADMIN")
+                        .requestMatchers("api/v1/product/private/save-product").hasRole("ADMIN")
+                        .requestMatchers("api/v1/product/private/update-product").hasRole("ADMIN")
+                        .requestMatchers("api/v1/order/private/save-order").hasRole("ADMIN")
+                        .requestMatchers("api/v1/order/private/update-order").hasRole("ADMIN")
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
