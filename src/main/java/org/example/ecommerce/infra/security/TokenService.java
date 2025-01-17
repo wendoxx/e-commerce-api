91ea41e12f211c902cea4a750c539f22762b958e
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-16 22:47:04 -0300

    fix: correct import in TokenService

--- src/main/java/org/example/ecommerce/infra/security/TokenService.java
+++ src/main/java/org/example/ecommerce/infra/security/TokenService.java
@@ -4,8 +4,8 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.example.ecommerce.model.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import java.security.interfaces.RSAPrivateKey;
