b009f6dbc937f5032a409ae702e6165366009f9a
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-01-28 22:54:43 -0300

    test: add new tests in AuthServiceTest

--- src/main/java/org/example/ecommerce/dto/request/LoginRequestDTO.java
+++ src/main/java/org/example/ecommerce/dto/request/LoginRequestDTO.java
@@ -1,7 +1,9 @@
 package org.example.ecommerce.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class LoginRequestDTO {
     private String username;
--- src/test/java/org/example/ecommerce/infra/security/AuthServiceTest.java
+++ src/test/java/org/example/ecommerce/infra/security/AuthServiceTest.java
@@ -1,27 +1,30 @@
 package org.example.ecommerce.infra.security;
 
+import org.example.ecommerce.dto.request.LoginRequestDTO;
 import org.example.ecommerce.dto.request.RegisterRequestDTO;
 import org.example.ecommerce.dto.response.RegisterResponseDTO;
+import org.example.ecommerce.infra.config.exception.UsernameIsNotAvailableException;
 import org.example.ecommerce.model.User;
 import org.example.ecommerce.reporitory.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
 
-    //UUID userID = UUID.randomUUID();
-    RegisterRequestDTO registerRequestDTO;
-
-    User savedUser;
 
     @InjectMocks
     AuthService authService;
@@ -29,10 +32,25 @@ public class AuthServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    RegisterRequestDTO registerRequestDTO;
+
+    @Mock
+    TokenService tokenService;
+
+    @Mock
+    AuthenticationManager authenticationManager;
+
+    @Mock
+    LoginRequestDTO loginRequestDTO;
+
+    User savedUser;
+
     @BeforeEach
     public void setUp() {
 
     registerRequestDTO = new RegisterRequestDTO("username1", "password1", UserRole.ADMIN);
+    loginRequestDTO = new LoginRequestDTO("username1", "password1");
 
     savedUser = new User();
     savedUser.setUsername("username1");
@@ -42,10 +60,21 @@ public class AuthServiceTest {
     }
 
     @Test
-    void shouldRegisterAnUser() {
+    @DisplayName("This test should register a user")
+    void shouldRegisterAUser() {
         RegisterResponseDTO result = authService.register(registerRequestDTO);
 
         assertEquals(result.getUsername(), savedUser.getUsername());
         assertEquals(result.getRole(), savedUser.getRole());
     }
+
+    @Test
+    @DisplayName("This test should throws an exception when the username isn't null")
+    void shouldThrowsAnExceptionWhenTheUsernameIsNotNullInRegister() {
+        when(userRepository.findByUsername(registerRequestDTO.getUsername())).thenReturn(savedUser);
+
+        assertEquals("username1", registerRequestDTO.getUsername());
+        assertThrows(UsernameIsNotAvailableException.class, () -> authService.register(registerRequestDTO));
+    }
+
 }
