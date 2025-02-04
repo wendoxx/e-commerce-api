de556b93838e59c7a9ce59acfc579ed4837653ee
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-03 23:45:41 -0300

    feat: add CartRepository

---
+++ src/main/java/org/example/ecommerce/reporitory/CartRepository.java
@@ -0,0 +1,11 @@
+package org.example.ecommerce.reporitory;
+
+import org.example.ecommerce.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+}
